56d069584bb53a96045e43b9d7e8959ac6cb85cc
Alex Stupakov <stupakov@gmail.com>
2016-12-18 18:13:55 -0800

    fix start_fadecandy script

--- scripts/start_fadecandy.sh
+++ scripts/start_fadecandy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-../src/fadecandy/server/fcserver ../config/fadecandy-config.json &
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+$DIR/src/fadecandy/server/fcserver $DIR/config/fadecandy-config.json &
